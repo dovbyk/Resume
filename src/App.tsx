0c0ca1cf2ab54a3acddbf109e3594d04679e399a
Aryan Gaire <botx711@gmail.com>
2025-02-13 22:03:55 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900">
+    <div className="min-h-screen bg-green-50">
       <Header />
       <main>
         <Hero />
